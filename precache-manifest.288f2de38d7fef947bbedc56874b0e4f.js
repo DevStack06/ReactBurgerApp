4485aa98c03bf8b2564a96189bebc9b0c8817f7a
balram0608 <balram0608@gmail.com>
2020-05-08 17:30:06 +0530

    Updates

---
+++ precache-manifest.288f2de38d7fef947bbedc56874b0e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3b28b8473af8f2cc532a2bab0b368f",
+    "url": "/ReactBurgerApp/index.html"
+  },
+  {
+    "revision": "f440b9c56fa495061ba4",
+    "url": "/ReactBurgerApp/static/css/main.1e54ac12.chunk.css"
+  },
+  {
+    "revision": "ca35fcc58f9163f2cd84",
+    "url": "/ReactBurgerApp/static/js/2.d1ecb0bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactBurgerApp/static/js/2.d1ecb0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f440b9c56fa495061ba4",
+    "url": "/ReactBurgerApp/static/js/main.1a80c33e.chunk.js"
+  },
+  {
+    "revision": "a806e84e84990bd045ee",
+    "url": "/ReactBurgerApp/static/js/runtime-main.6b727ca5.js"
+  }
+]);
